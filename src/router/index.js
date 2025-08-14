94ba407ca140237912fdba2d79e883947e926afb
219 <lhg2196@naver.com>
2025-08-14 12:53:24 +0900

    세팅완료

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { creatRouter, createWebHistory} from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        }
+    ]
+})
+
+export default router
